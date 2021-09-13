1d5c95242e28e6f2bba33a239660408be0499a2f
teefzizzle <36226936+teefzizzle@users.noreply.github.com>
2021-09-13 13:14:32 +1200

    Add files via upload

--- Simple SIR.py
+++ Simple SIR.py
@@ -16,9 +16,16 @@ I0 = 1 - S0  # initial infected
 R0 = 1 - S0 - I0  # initial removed
 beta = 0.35  # rate of infection
 gamma = 0.1  # recovery rate
-T = np.linspace(0, 100, 1000)
+T = np.linspace(0, 100, 101)
+N = 5e6
+hospitalisation_rate = 0.2
 
 solution = odeint(SIR_model, [S0, I0,R0], T, args=(beta, gamma))
+
+print("Final Numbers \nSusceptable: ", solution[-1:, 0], "\nInfected: ",
+      solution[-1:, 1], "\nRemoved: ", solution[-1:, 2], "\nPeak infections: ",
+      max(solution[:, 1]) * N, "\nPeak immunity: ", max(solution[:, 2]) * N,
+      "\nPeak in hospital: ", max(solution[:, 1] * N * hospitalisation_rate))
 
 plt.figure(figsize=[8, 6])
 plt.plot(T, solution[:, 0], label="S(t)")
@@ -27,5 +34,5 @@ plt.plot(T, solution[:, 2], label="R(t)")
 plt.legend()
 plt.xlabel("Time")
 plt.ylabel("Proportion")
-plt.title('SIR Model')
+plt.title('Basic Case SIR Model')
 plt.show()
